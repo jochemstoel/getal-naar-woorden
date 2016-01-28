e8469cf084cf76562c0b026e56001870ba360147
Jochem Stoel <jochemstoel@gmail.com>
2016-01-28 17:51:58 +0100

    Firt commit
    Dit is mijn eerste commit van deze repo op 28 januari 2016.

---
+++ NumberParser.php
@@ -0,0 +1,167 @@
+<?php
+/* (PHP) Converteer getal naar linguïstische expressie
+ *
+ * @package getal-naar-woorden
+ * @url https://github.com/jochemstoel/getal-naar-woorden
+ * @author Jochem Stoel (http://jochemstoel.github.io)
+ * @version 1.0
+ * @license Don't be a jerk
+ */
+
+class NumberParser{
+
+      protected $_smallNumberMapping = array(
+            0 => 'nul',
+            1 => 'een',
+            2 => 'twee',
+            3 => 'drie',
+            4 => 'vier',
+            5 => 'vijf',
+            6 => 'zes',
+            7 => 'zeven',
+            8 => 'acht',
+            9 => 'negen',
+            10 => 'tien',
+            11 => 'elf',
+            12 => 'twaalf',
+            13 => 'dertien',
+            14 => 'veertien',
+            15 => 'vijftien',
+            16 => 'zestien',
+            17 => 'zeventien',
+            18 => 'achttien',
+            19 => 'negentien',
+            20 => 'twintig',
+            21 => 'eenentwintig',
+            22 => 'tweeëntwintig',
+            23 => 'drieëntwintig',
+            24 => 'vierentwintig',
+            25 => 'vijfentwintig',
+            26 => 'zesentwintig',
+            27 => 'zevenentwintig',
+            28 => 'achtentwintig',
+            29 => 'negenentwintig',
+            30 => 'dertig',
+            31 => 'eenendertig',
+            32 => 'tweeëndertig',
+            33 => 'drieëndertig',
+            34 => 'vierendertig',
+            35 => 'vijfendertig',
+            36 => 'zesendertig',
+            37 => 'zevenendertig',
+            38 => 'achtendertig',
+            39 => 'negenendertig',
+            40 => 'veertig',
+            41 => 'eenenveertig',
+            42 => 'tweeënveertig',
+            43 => 'drieënveertig',
+            44 => 'vierenveertig',
+            45 => 'vijfenveertig',
+            46 => 'zesenveertig',
+            47 => 'zevenenveertig',
+            48 => 'achtenveertig',
+            49 => 'negenenveertig',
+            50 => 'vijftig',
+            51 => 'eenenvijftig',
+            52 => 'tweeënvijftig',
+            53 => 'drieënvijftig',
+            54 => 'vierenvijftig',
+            55 => 'vijfenvijftig',
+            56 => 'zesenvijftig',
+            57 => 'zevenenvijftig',
+            58 => 'achtenvijftig',
+            59 => 'negenenvijftig',
+            60 => 'zestig',
+            61 => 'eenenzestig',
+            62 => 'tweeënzestig',
+            63 => 'drieënzestig',
+            64 => 'vierenzestig',
+            65 => 'vijfenzestig',
+            66 => 'zesenzestig',
+            67 => 'zevenenzestig',
+            68 => 'achtenzestig',
+            69 => 'negenenzestig',
+            70 => 'zeventig',
+            71 => 'eenenzeventig',
+            72 => 'tweeënzeventig',
+            73 => 'drieënzeventig',
+            74 => 'vierenzeventig',
+            75 => 'vijfenzeventig',
+            76 => 'zesenzeventig',
+            77 => 'zevenenzeventig',
+            78 => 'achtenzeventig',
+            79 => 'negenenzeventig',
+            80 => 'tachtig',
+            81 => 'eenentachtig',
+            82 => 'tweeëntachtig',
+            83 => 'drieëntachtig',
+            84 => 'vierentachtig',
+            85 => 'vijfentachtig',
+            86 => 'zesentachtig',
+            87 => 'zevenentachtig',
+            88 => 'achtentachtig',
+            89 => 'negenentachtig',
+            90 => 'negentig',
+            91 => 'eenennegentig',
+            92 => 'tweeënnegentig',
+            93 => 'drieënnegentig',
+            94 => 'vierennegentig',
+            95 => 'vijfennegentig',
+            96 => 'zesennegentig',
+            97 => 'zevenennegentig',
+            98 => 'achtennegentig',
+            99 => 'negenennegentig'
+      );
+
+      public function __construct(){}
+
+      public function parseNumber($number){
+            if($number === 0) return 'nul';
+            if($number > 1000000000000) throw new Exception('Number must be smaller than 1000 000 000 000.');
+
+            $result = '';
+
+            if($number > 1000000000){
+                  $rest = fmod($number, 1000000000);
+                  $billions = ($number - $rest) / 1000000000;
+                  $number = $rest;
+                  $result .= $this->parseSmallNumber($billions) . ' miljard ';
+            }
+
+            if($number > 1000000){
+                  $rest = fmod($number, 1000000);
+                  $millions = ($number - $rest) / 1000000;
+                  $number = $rest;
+                  $result .= $this->parseSmallNumber($millions)  . ' miljoen ';
+            }
+
+            if($number >= 1000){
+                  $rest = fmod($number,1000);
+                  $thousands = ($number - $rest) / 1000;
+                  $number = $rest;
+                  if($thousands > 1) $result .= $this->parseSmallNumber($thousands);
+                  $result .= 'duizend ';
+            }
+
+            $rest = $number - intval($number);
+            $number = $number - $rest;
+            $result .= $this->parseSmallNumber($number);
+
+            //honderdsten apart doen      
+            if($rest > 0) $result .= ' en ' . $this->parseSmallNumber(round($rest * 100)) . ' honderdsten';
+
+            return trim($result);
+      }
+
+      private function parseSmallNumber($smallNumber){
+            if($smallNumber >= 1000) throw new Exception('Number must be smaller than 1000.');
+            if($smallNumber < 100){
+                  return $this->_smallNumberMapping[$smallNumber];
+            }
+            $tenthsAndOnes = $smallNumber % 100;
+            $hundreds = ($smallNumber - $tenthsAndOnes) / 100;
+            $hundredsString = $hundreds != 1 ? $this->_smallNumberMapping[$hundreds] : '';
+            return $hundredsString . 'honderd' . $this->_smallNumberMapping[$tenthsAndOnes];
+      }
+}
+?>
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# getal-naar-woorden
-Vertaal een getal naar de linguïstische expressie. (18527 naar 'achttienduizend vijfhonderdzevenentwintig')
+# NumberParser 
+Ik zoek 'even snel' met Google naar een PHP functie die een getal converteert naar woorden. Ik vind voornamelijk topics waarin mensen vragen hoe dat moet. In enkele gevallen zit daar een stukje brakke code bij die voor de helft werkt. De oplossingen die mensen geven zijn treurig. Het feit dat deze functie niet even zo makkelijk te vinden is op het internet is een ernstig probleem in de wereld dat ik absoluut niet aan kan zien en ik voel de morele verplichting om een oplossing te bieden. Voor de zoveelste keer betekent 'even snel' iets opzoeken dat ik mijn kostbare tijd opoffer aan de maatschappij.
+
+De PHP class in deze repository is eigenlijk de NumberParser Service class op <a href="https://github.com/TijdschriftVoorGeneeskunde/TijdschriftVoorGeneeskunde/blob/fbc745993d36d483ffa1dd1c35524b11012349f2/src/AppBundle/Service/NumberParser.php">https://github.com/TijdschriftVoorGeneeskunde/TijdschriftVoorGeneeskunde/blob/fbc745993d36d483ffa1dd1c35524b11012349f2/src/AppBundle/Service/NumberParser.php</a>, door mij aangepast om namespace/applicatie onafhankelijk te werken.
+
+Ik heb NumberParser herschreven in een aantal talen / vormen
+* NumberParser PHP Class
+* NumberParser Javascript Object
+* NumberParser Node.js / NPM module
+
+### PHP Voorbeeld
+Gebruik NumberParser in je PHP script.
+```
+include 'NumberParser.php';
+
+$numparser  = new NumberParser();
+print $numparser->parseNumber(7564928); // zeven miljoen vijfhonderdvierenzestigduizend negenhonderdachtentwintig
+```
+
+### Javascript Voorbeeld
+```javascript
+NumberParser.parseNumber(7564928); // zeven miljoen vijfhonderdvierenzestigduizend negenhonderdachtentwintig
+```
+
+### Webpagina Voorbeeld
+NumberParser insluiten in de head van je webpagina om het te kunnen gebruiken.
+```html
+<script type="application/javascript" src="NumberParser.js"></script>
+<script type="application/javascript">
+	NumberParser.parseNumber(7564928); // zeven miljoen vijfhonderdvierenzestigduizend negenhonderdachtentwintig
+</script>
+```
+
+### Node.js / NPM require
+Ik heb helemaal niets beters te doen en daarom is NumberParser ook als Node module inbegrepen. Het bestand NumberParser.Node.js is identiek aan NumberParser.js met als enige verschil dat het object toegewezen is aan module.exports zodat het kan worden ge-required.
+```javascript
+var NumberParser = require('NumberParser');
+NumberParser.parseNumber(7564928); // zeven miljoen vijfhonderdvierenzestigduizend negenhonderdachtentwintig
+```
+
+## Opmerkingen
+Omdat ik overal commentaar op heb.
+
+#### fmod
+Javascript heeft geen fmod functie. GitHub gebruiker wteuber schreef een Javascript equivalent van PHP fmod (inbegrepen)
+```javascript
+/* https://gist.github.com/wteuber/6241786 */
+function fmod(a,b) { return Number((a - (Math.floor(a / b) * b)).toPrecision(8)); };
+```
+
+#### Overig
+Binnen ennkele tijd voeg ik nog een NumberParser toe geschreven in C# en C++ en zal ik tevens NumberParser er ook bij doen als Shared Library (.dll) voor in je .NET applicatie.
